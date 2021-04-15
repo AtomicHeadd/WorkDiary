856dc7aea073a6edd8d747eacd2c5f9446d2a77f
MonstersoulDev1 <58561188+MonstersoulDev1@users.noreply.github.com>
2021-04-15 19:26:05 +0900

    仮の統計画面を実装 その日のタスクごとの合計時間のみ確認可能

--- WorkDiary.py
+++ WorkDiary.py
@@ -58,6 +58,7 @@ class recorder():
         self.button2.configure(text=self.button_name[1])
         self.button3.configure(text=self.button_name[2])
         self.current_target.configure(text="")
+        self.load_data()
 
     def button1_clicked(self):
         self.save_recording()
@@ -109,6 +110,39 @@ class recorder():
             self.current_target.configure(text="")
         self.root.after(1000, self.update_clock)
 
+    def load_data(self, target=time.strftime("%Y-%m-%d").split("-")):
+        a = dict()
+        with open("DiaryRecord.csv", "r") as f:
+            for line in f:
+                line_info = re.match(r"(\d\d\d\d-\d+-\d+).+,\s(.+),\s(\d+):(\d+)", line)
+                if line_info is None:
+                    continue
+                date_num = [int(s) for s in line_info.group(1).split("-")]
+                if date_num == target:
+                    #既に登録されている場合
+                    task_name = line_info.group(2)
+                    if task_name in a:
+                        a[task_name] = [int(x)+int(y) for (x, y) in zip(a[task_name], [line_info.group(3), line_info.group(4)])]
+                    #無い場合
+                    else:
+                        a[task_name] = [line_info.group(3), line_info.group(4)]
+        if target == time.strftime("%Y-%m-%d").split("-"):
+            stats_content = "本日\n"
+        else:
+            stats_content = f"{target[0]}/{target[1]}/{target[2]}\n"
+        if len(a) == 0:
+            stats_content += "活動なし"
+            self.stats_text.configure(text=stats_content)
+            return
+        for key, value in a.items():
+            up = int(value[1] / 60)
+            rest = value[1] % 60
+            if value[0] + up < 1:
+                stats_content += key + ": " + str(value[1]) + "分\n"
+            else:
+                stats_content += key + ": " + str(up) + "時間" + str(rest) + "分\n"
+        self.stats_text.configure(text=stats_content)
+
     def __init__(self):
         if hasattr(sys, 'setdefaultencoding'):
             import locale
@@ -125,16 +159,16 @@ class recorder():
         # ウィンドウ作成
         self.root.title("作業レコーダー")
         self.root.geometry("400x400")
-        self.root.minsize(width=370, height=200)
-        self.root.maxsize(width=370, height=200)
+        self.root.minsize(width=370, height=250)
+        self.root.maxsize(width=370, height=250)
 
         # ボタン等
-        setting = tk.Button(self.root, text="⚙")
-        setting.grid(row=0, column=0)
-        take_time = tk.Button(self.root, text="測定")
-        take_time.grid(row=0, column=1)
-        stats = tk.Button(self.root, text="統計")
-        stats.grid(row=0, column=2)
+        tab_setting = tk.Button(self.root, text="⚙")
+        tab_setting.grid(row=0, column=0)
+        tab_watch = tk.Button(self.root, text="測定")
+        tab_watch.grid(row=0, column=1)
+        tab_stats = tk.Button(self.root, text="統計")
+        tab_stats.grid(row=0, column=2)
         self.button1 = tk.Button(self.root, text=self.button_name[0], height=5, width=10, command=self.button1_clicked)
         self.button1.place(x=10, y=40)
         self.button2 = tk.Button(self.root, text=self.button_name[1], height=5, width=10, command=self.button2_clicked)
@@ -144,7 +178,15 @@ class recorder():
         self.current_target_index = -1
         self.current_target = tk.Label(text="")
         self.current_target.place(x=0, y=130)
+        #統計ボタン
+        self.stats_left = tk.Button(self.root, text="←", height=1, width=1)
+        self.stats_left.place(x=10, y=180)
+        self.stats_right = tk.Button(self.root, text="→", height=1, width=1)
+        self.stats_right.place(x=340, y=180)
+        self.stats_text = tk.Label(text="本日")
+        self.stats_text.place(x=140, y=150)
 
+        self.load_data()
         self.update_clock()
 
         self.root.mainloop()
